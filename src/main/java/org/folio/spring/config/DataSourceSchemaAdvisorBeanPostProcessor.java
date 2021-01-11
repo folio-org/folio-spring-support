92c2e242ba703bdfb612b3f579244bc7923eb7b8
Mikhail Fokanov <61314589+MikhailFokanov@users.noreply.github.com>
2021-01-11 17:59:35 +0300

    Fix spring data source adviser for proxy classes

--- src/main/java/org/folio/spring/config/DataSourceSchemaAdvisorBeanPostProcessor.java
+++ src/main/java/org/folio/spring/config/DataSourceSchemaAdvisorBeanPostProcessor.java
@@ -1,12 +1,15 @@
 package org.folio.spring.config;
 
+import javax.sql.DataSource;
 import org.folio.spring.FolioExecutionContext;
+import org.springframework.aop.scope.ScopedProxyFactoryBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
-import javax.sql.DataSource;
-
 public class DataSourceSchemaAdvisorBeanPostProcessor implements BeanPostProcessor {
+
   private final FolioExecutionContext folioExecutionContext;
+
+  public static final String DATASOURCE_BEAN_NAME = "dataSource";
 
   public DataSourceSchemaAdvisorBeanPostProcessor(FolioExecutionContext folioExecutionContext) {
     this.folioExecutionContext = folioExecutionContext;
@@ -14,8 +17,19 @@ public class DataSourceSchemaAdvisorBeanPostProcessor implements BeanPostProcess
 
   @Override
   public Object postProcessAfterInitialization(Object bean, String beanName) {
-    if ("dataSource".equals(beanName)) {
-      return new DataSourceFolioWrapper((DataSource) bean, folioExecutionContext);
+    if (DATASOURCE_BEAN_NAME.equals(beanName)) {
+      DataSource dataSource;
+      if (bean instanceof ScopedProxyFactoryBean) {
+        if (((ScopedProxyFactoryBean) bean).getObject() instanceof DataSource) {
+          dataSource = (DataSource) ((ScopedProxyFactoryBean) bean).getObject();
+        } else {
+          throw new IllegalStateException(
+            "Bean with dataSource name should be instance of DataSource");
+        }
+      } else {
+        dataSource = (DataSource) bean;
+      }
+      return new DataSourceFolioWrapper(dataSource, folioExecutionContext);
     } else {
       return bean;
     }
