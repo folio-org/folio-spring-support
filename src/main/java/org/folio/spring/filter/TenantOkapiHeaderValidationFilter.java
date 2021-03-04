2b6fbf140e8244f66590609ede98a7c2c25f178a
Craig McNally <cmcnally@ebsco.com>
2021-03-04 08:31:31 -0500

    FOLSPRINGB-13 Don't require x-okapi-tenant for admin endpoints (#15)
    * FOLSPRINGB-13 admin endpoints shouldn't require x-okapi-tenant
    
    * FOLSPRINGB-13 make configurable and test
    
    * FOLSPRINGB-13 accidental commit of stub test
    
    * FOLSPRINGB-13 support multiple exclusions from x-okapi-tenant header validation
    
    * FOLSPRINGB-13 address PR comments
    
    * FOLSPRINGB-13 fixing api-schema-lint errors
    
    * Simplify a bit and improve efficiency
    
    * Remove duplicated and empty lines
    
    * Use the new error constant
    
    * Update README.md
    
    Co-authored-by: Hongwei Ji <hji@ebsco.com>

--- src/main/java/org/folio/spring/filter/TenantOkapiHeaderValidationFilter.java
+++ src/main/java/org/folio/spring/filter/TenantOkapiHeaderValidationFilter.java
@@ -2,11 +2,11 @@ package org.folio.spring.filter;
 
 import org.apache.commons.lang3.StringUtils;
 import org.folio.spring.integration.XOkapiHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -14,21 +14,29 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component("defaultTenantOkapiHeaderValidationFilter")
 @ConditionalOnMissingBean(name = "tenantOkapiHeaderValidationFilter")
 @ConditionalOnProperty(prefix = "folio.tenant.validation", name = "enabled", matchIfMissing = true)
 public class TenantOkapiHeaderValidationFilter extends GenericFilterBean {
+
+  public static final String ERROR_MSG = XOkapiHeaders.TENANT + " header must be provided";
+
+  @Value("${header.validation.x-okapi-tenant.exclude.base-paths:/admin}")
+  private String[] excludeBasePaths;
+
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     HttpServletRequest req = (HttpServletRequest) request;
-    if (StringUtils.isBlank(req.getHeader(XOkapiHeaders.TENANT))) {
-      var res = (HttpServletResponse) response;
+    if (StringUtils.isNotBlank(req.getHeader(XOkapiHeaders.TENANT)) || Arrays.stream(excludeBasePaths)
+      .anyMatch(req.getRequestURI()::startsWith)) {
+      chain.doFilter(request, response);
+    } else {
+      var res = ((HttpServletResponse) response);
       res.setContentType("text/plain");
       res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-      res.getWriter().println(XOkapiHeaders.TENANT + " header must be provided");
-    } else {
-      chain.doFilter(request, response);
+      res.getWriter().println(ERROR_MSG);
     }
   }
 }
