b0405e0ef1e171ac3440dc93c3ccd13b82bd3e9c
nirmalr-shah <nirmal_shah@epam.com>
2023-08-30 14:25:27 +0530

    FOLSPRINGB-118 : updated path and package of SystemUserConfig

--- folio-spring-system-user/src/main/java/org/folio/spring/config/SystemUserConfig.java
+++ folio-spring-system-user/src/main/java/org/folio/spring/config/SystemUserConfig.java
@@ -1,4 +1,4 @@
-package org.folio.spring.config.properties;
+package org.folio.spring.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
