605d8697a9ee1a7b3ab0c8e197c26754dcede606
julianladisch <julianladisch@users.noreply.github.com>
2023-11-01 13:41:09 +0100

    FOLSPRINGB-126: Reject empty and null system user password (#116)
    The folio-spring-system-user library should reject an empty or null system user password.
    
    Use case:
    
    As a sysop I may accidentially forget to set the environment variable.
    
    For example forgetting to add SYSTEM_USER_PASSWORD, or forget to add the secret to a deployment in Rancher, or having a typo in the variable name like SYSTEM_USERS_PASSWORD.
    
    When the system user password is not set or is empty the attempt to create the system user when enabling the module for a tenant (or upgrading the module for a tenant) should fail with an error message about the missing password.
    
    The module MUST NOT set an empty password.
    
    This is security by default and is required for GDPR compliance (GDPR Article 25 "Data protection by design and by default").
    
    Therefore folio-spring-system-user MUST reject a system user with null or empty password.
    
    (cherry picked from commit 6e9b3e757086ea517480e587774ddeb1de6bf21f)

--- folio-spring-system-user/src/main/java/org/folio/spring/service/SystemUserProperties.java
+++ folio-spring-system-user/src/main/java/org/folio/spring/service/SystemUserProperties.java
@@ -1,7 +1,13 @@
 package org.folio.spring.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("folio.system-user")
 public record SystemUserProperties(String username, String password, String lastname, String permissionsFilePath) {
+  public SystemUserProperties {
+    if (StringUtils.isEmpty(password)) {
+      throw new IllegalArgumentException("system user password must be configured to be non-empty");
+    }
+  }
 }
