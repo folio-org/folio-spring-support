1a874bb5891a5a2d823965f2fdba520ad5d7df70
Noah Overcash <novercash@ebsco.com>
2023-11-10 15:25:04 -0600

    Add locale default tests

--- folio-spring-i18n/src/test/java/org/folio/spring/service/TranslationServiceResolutionTest.java
+++ folio-spring-i18n/src/test/java/org/folio/spring/service/TranslationServiceResolutionTest.java
@@ -3,7 +3,9 @@ package org.folio.spring.service;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
 import java.util.Locale;
 import org.folio.spring.config.TranslationConfiguration;
 import org.junit.jupiter.api.Test;
@@ -56,6 +58,77 @@ class TranslationServiceResolutionTest {
     assertThat(
       service.getTranslation(Locale.UK, null).get("mod-foo.en_ca_only"),
       is("mod-foo.en_ca_only")
+    );
+  }
+
+  @Test
+  void testDefaultLocale() {
+    TranslationService service = new TranslationService(
+      new PathMatchingResourcePatternResolver(),
+      new TranslationConfiguration(
+        "/test-translations/test-normal/",
+        new Locale("test", "")
+      )
+    );
+
+    assertThat(service.getCurrentLocale(), is(new Locale("test", "")));
+
+    assertThat(
+      service.getDefaultTranslation().getLocale(),
+      is(new Locale("test", ""))
+    );
+  }
+
+  @Test
+  void testDefaultLocaleFallback() {
+    TranslationService service = getService("multiple");
+
+    Locale.setDefault(new Locale("test", ""));
+    assertThat(service.getDefaultTranslation().getLocale(), is(Locale.ENGLISH));
+  }
+
+  @Test
+  void testDefaultLocaleException() {
+    TranslationService service = getService("normal");
+
+    // only available are test.json, so FR_FR and EN_US don't match
+    Locale.setDefault(Locale.FRANCE);
+    assertThrows(
+      IllegalStateException.class,
+      () -> service.getDefaultTranslation(),
+      "No available translations causes an IllegalStateException"
+    );
+  }
+
+  @Test
+  void testBestTranslation() {
+    TranslationService service = getService("multiple");
+
+    assertThat(
+      service
+        .getBestTranslation(Arrays.asList(Locale.US, Locale.FRANCE))
+        .getLocale(),
+      is(Locale.US)
+    );
+    assertThat(
+      service
+        .getBestTranslation(Arrays.asList(Locale.FRANCE, Locale.US))
+        .getLocale(),
+      is(Locale.FRANCE)
+    );
+    assertThat(
+      service
+        .getBestTranslation(Arrays.asList(Locale.CHINESE, Locale.US))
+        .getLocale(),
+      is(Locale.US)
+    );
+    assertThat(
+      service.getBestTranslation(Arrays.asList(Locale.CHINESE)).getLocale(),
+      is(Locale.ENGLISH) // server default
+    );
+    assertThat(
+      service.getBestTranslation(Arrays.asList()).getLocale(),
+      is(Locale.ENGLISH) // server default
     );
   }
 }
