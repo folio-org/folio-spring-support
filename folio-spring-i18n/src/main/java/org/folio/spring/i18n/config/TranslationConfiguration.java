1077d38a6d0a0a9ac246f8b68f338eb2ddacc1c9
Noah Overcash <novercash@ebsco.com>
2023-11-20 17:40:46 -0500

    Use AutoConfiguration instead of Configuration Seems to be a newer Spring requirement: https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.developing-auto-configuration.understanding-auto-configured-beans

--- folio-spring-i18n/src/main/java/org/folio/spring/i18n/config/TranslationConfiguration.java
+++ folio-spring-i18n/src/main/java/org/folio/spring/i18n/config/TranslationConfiguration.java
@@ -4,15 +4,15 @@ import java.util.Locale;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * An {@link org.springframework.context.annotation.Configuration Configuration} bean to hold the
  * location of the current translation directory and default fallback locale.
  */
 @Getter
-@Configuration
+@AutoConfiguration
 @ComponentScan(basePackages = "org.folio.spring.i18n.service")
 public class TranslationConfiguration {
 
