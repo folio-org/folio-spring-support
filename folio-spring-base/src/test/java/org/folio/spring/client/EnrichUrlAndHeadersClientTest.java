da49c9d71147dc693f34cd8e2bb91fdda0e80a4c
Noah Overcash <novercash@ebsco.com>
2024-01-09 10:29:52 -0500

    More tests

--- folio-spring-base/src/test/java/org/folio/spring/client/EnrichUrlAndHeadersClientTest.java
+++ folio-spring-base/src/test/java/org/folio/spring/client/EnrichUrlAndHeadersClientTest.java
@@ -6,10 +6,17 @@ import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import feign.Request;
+import feign.Response;
+import feign.okhttp.OkHttpClient;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +26,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.ArgumentCaptor;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @UnitTest
 class EnrichUrlAndHeadersClientTest {
@@ -118,6 +127,73 @@ class EnrichUrlAndHeadersClientTest {
           hasEntry("z", List.of("z-val")),
           hasEntry("Accept-Language", List.of("en-US,en;q=0.9")),
           is(aMapWithSize(5))
+        )
+      )
+    );
+  }
+
+  @Test
+  void testRequestExecution() throws IOException {
+    Request request = Request.create(
+      Request.HttpMethod.GET,
+      "http://test-url",
+      Map.of("a", List.of("a-val")),
+      Request.Body.create("test-data"),
+      null
+    );
+
+    FolioExecutionContext context = mock(FolioExecutionContext.class);
+    when(context.getOkapiUrl()).thenReturn("http://okapi");
+    when(context.getOkapiHeaders()).thenReturn(Map.of("z", List.of("z-val")));
+    when(context.getAllHeaders())
+      .thenReturn(
+        Map.of(
+          "misc-1",
+          List.of("misc-1-val"),
+          "accept-language",
+          List.of("en-US,en;q=0.9"),
+          "misc-2",
+          List.of("misc-2-val")
+        )
+      );
+
+    Request.Options options = new Request.Options();
+
+    EnrichUrlAndHeadersClient client = new EnrichUrlAndHeadersClient(
+      context,
+      null
+    );
+
+    // best way to check for this without having to do a bunch of feign/okhttp logic
+    OkHttpClient mockedDelegate = mock(OkHttpClient.class);
+    ReflectionTestUtils.setField(client, "delegate", mockedDelegate);
+
+    when(mockedDelegate.execute(any(Request.class), eq(options)))
+      .thenReturn(Response.builder().request(request).status(299).build());
+
+    // ensure response is returned upon execution
+    assertThat(client.execute(request, options).status(), is(299));
+
+    // verify that the correct request was sent with the correct options
+    ArgumentCaptor<Request> requestCaptor = ArgumentCaptor.forClass(
+      Request.class
+    );
+    verify(mockedDelegate, times(1))
+      .execute(requestCaptor.capture(), eq(options));
+
+    assertThat(
+      requestCaptor.getValue().httpMethod(),
+      is(Request.HttpMethod.GET)
+    );
+    assertThat(requestCaptor.getValue().url(), is("http://okapi/test-url"));
+    assertThat(
+      requestCaptor.getValue().headers(),
+      is(
+        allOf(
+          hasEntry("a", List.of("a-val")),
+          hasEntry("z", List.of("z-val")),
+          hasEntry("Accept-Language", List.of("en-US,en;q=0.9")),
+          is(aMapWithSize(3))
         )
       )
     );
