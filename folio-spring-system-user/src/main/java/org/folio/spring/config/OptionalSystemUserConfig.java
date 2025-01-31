7a821a68920bd0652aa3dc89d7d8b2351cbbcd78
Noah Overcash <novercash@ebsco.com>
2025-01-31 09:15:25 -0500

    [FOLSPRINGS-183] Fix initialization when system user is disabled (#224)
    * [FOLSPRINGS-183] Fix initialization when system user is disabled
    
    * Add test coverage

--- folio-spring-system-user/src/main/java/org/folio/spring/config/OptionalSystemUserConfig.java
+++ folio-spring-system-user/src/main/java/org/folio/spring/config/OptionalSystemUserConfig.java
@@ -2,11 +2,13 @@ package org.folio.spring.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.client.AuthnClient;
 import org.folio.spring.client.UsersClient;
 import org.folio.spring.config.properties.FolioEnvironment;
 import org.folio.spring.context.ExecutionContextBuilder;
 import org.folio.spring.model.SystemUser;
+import org.folio.spring.service.PrepareSystemUserService;
 import org.folio.spring.service.SystemUserProperties;
 import org.folio.spring.service.SystemUserService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -33,5 +35,11 @@ public class OptionalSystemUserConfig {
       AuthnClient authnClient, UsersClient usersClient) {
     return new SystemUserService(executionContextBuilder, systemUserProperties, folioEnvironment,
       authnClient, usersClient);
+  }
+
+  @Bean
+  public PrepareSystemUserService prepareSystemUserService(FolioExecutionContext executionContext,
+      SystemUserProperties systemUserProperties, SystemUserService systemUserService) {
+    return new PrepareSystemUserService(executionContext, systemUserProperties, systemUserService);
   }
 }
