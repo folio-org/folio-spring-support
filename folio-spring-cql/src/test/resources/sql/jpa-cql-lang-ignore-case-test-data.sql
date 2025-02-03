59fb3fac90cdc54893c97bf6478595356b40c7d3
Antony_Hruschev <anton.gitgit@gmail.com>
2025-02-03 20:31:10 +0400

    MODREQMED-151: case insensitive accents ignoring CQL: add test cases
    (cherry picked from commit 4c5c89460f2c0b8e9bcb88b7d6b303344b483452)

---
+++ folio-spring-cql/src/test/resources/sql/jpa-cql-lang-ignore-case-test-data.sql
@@ -0,0 +1,4 @@
+insert into lang(id, name) values (204, 'Java');
+insert into lang(id, name) values (205, 'jAva');
+insert into lang(id, name) values (206, 'JaVa');
+insert into lang(id, name) values (207, 'javA');
