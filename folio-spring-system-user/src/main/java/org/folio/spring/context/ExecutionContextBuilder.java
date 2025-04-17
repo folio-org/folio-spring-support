e3df1b05ce5a827d28de5a0867ea924edaf29ce2
viacheslav_kolesnyk <Viacheslav_Kolesnyk@epam.com>
2025-04-17 18:41:41 +0200

    feat(system-user): Add headers parameter to executeSystemUserScoped method
    - Extend ExecutionContextBuilder with a method accepting headers
    - Extend SystemUserScopedExecutionService with a method accepting headers
    
    Implements: FOLSPRINGS-195

--- folio-spring-system-user/src/main/java/org/folio/spring/context/ExecutionContextBuilder.java
+++ folio-spring-system-user/src/main/java/org/folio/spring/context/ExecutionContextBuilder.java
@@ -2,10 +2,14 @@ package org.folio.spring.context;
 
 import static java.util.Collections.singleton;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import javax.annotation.CheckForNull;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.folio.spring.DefaultFolioExecutionContext;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.FolioModuleMetadata;
@@ -37,6 +41,21 @@ public class ExecutionContextBuilder {
     return new DefaultFolioExecutionContext(
       moduleMetadata,
       Map.of(XOkapiHeaders.URL, singleton(okapiUrl), XOkapiHeaders.TENANT, singleton(tenantId))
+    );
+  }
+
+  public FolioExecutionContext buildContext(String tenantId, Map<String, Collection<String>> headers) {
+    var okapiUrl = folioEnvironment.getOkapiUrl();
+    var contextHeaders = headers == null
+      ? new HashMap<String, Collection<String>>()
+      : headers.entrySet().stream()
+      .filter(entry -> CollectionUtils.isNotEmpty(entry.getValue()))
+      .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    contextHeaders.put(XOkapiHeaders.URL, singleton(okapiUrl));
+    contextHeaders.put(XOkapiHeaders.TENANT, singleton(tenantId));
+    return new DefaultFolioExecutionContext(
+      moduleMetadata,
+      contextHeaders
     );
   }
 }
