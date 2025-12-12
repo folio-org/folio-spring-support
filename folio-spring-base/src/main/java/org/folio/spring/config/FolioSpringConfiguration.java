88c01f5b0034c3ad746cf564710e740379e2bb27
Pavel Filippov <pavel_filippov@epam.com>
2025-12-12 17:50:10 +0200

    FOLIO-4381 Implement connection pool settings

--- folio-spring-base/src/main/java/org/folio/spring/config/FolioSpringConfiguration.java
+++ folio-spring-base/src/main/java/org/folio/spring/config/FolioSpringConfiguration.java
@@ -51,8 +51,9 @@ public class FolioSpringConfiguration {
   @Bean
   @Qualifier("dataSourceSchemaAdvisorBeanPostProcessor")
   public static BeanPostProcessor dataSourceSchemaAdvisorBeanPostProcessor(
+    @Autowired FolioModuleMetadata moduleMetadata,
     @Autowired @Lazy FolioExecutionContext folioExecutionContext) {
-    return new DataSourceSchemaAdvisorBeanPostProcessor(folioExecutionContext);
+    return new DataSourceSchemaAdvisorBeanPostProcessor(folioExecutionContext, moduleMetadata);
   }
 
 }
