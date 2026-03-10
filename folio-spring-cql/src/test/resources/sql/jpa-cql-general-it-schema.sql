18000a66766443563c61801a8fe2a442cd0d781a
Pavlo Smahin <Pavlo_Smahin@epam.com>
2026-03-10 10:40:37 +0200

    FOLSPRINGS-221: Support CQL queries on @ElementCollection fields in Cql2JpaCriteria (#315)
    * FOLSPRINGS-221: Support CQL queries on @ElementCollection fields in Cql2JpaCriteria
    
    * - add unit tests for Cql2JpaCriteria

--- folio-spring-cql/src/test/resources/sql/jpa-cql-general-it-schema.sql
+++ folio-spring-cql/src/test/resources/sql/jpa-cql-general-it-schema.sql
@@ -7,9 +7,13 @@ DROP TABLE IF EXISTS
   city,
   person,
   str,
-  lang;
+  lang,
+  grp_member,
+  grp;
 
 CREATE TABLE city(id INT PRIMARY KEY, name VARCHAR(255));
 CREATE TABLE person(id INT PRIMARY KEY, name VARCHAR(255), age INT, identifier UUID, is_alive boolean, date_born timestamp, local_date timestamp, city_id INT REFERENCES city(id), deleted boolean, created_date timestamp);
 CREATE TABLE str(id INT PRIMARY KEY, str text);
 CREATE TABLE lang(id INT PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE grp(id UUID PRIMARY KEY, name VARCHAR(255) NOT NULL);
+CREATE TABLE grp_member(group_id UUID NOT NULL REFERENCES grp(id), member_id UUID NOT NULL);
